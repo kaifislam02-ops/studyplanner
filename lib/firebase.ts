aac9c648068c169c4c6f5c373d835047135f7687
Kaif Islam <kaifislam02ops@gmail.com>
2025-11-22 18:28:30 +0530

    fix: Correct Firebase configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.AIzaSyCltoMxKACnSksAI3Pk_Dl87sK_s6Vs3GM,
-  authDomain: process.env.studyplanner-f5de6.firebaseapp.com,
-  projectId: process.env.studyplanner-f5de6,
-  storageBucket: process.env.studyplanner-f5de6.firebasestorage.app,
-  messagingSenderId: process.env.283699367756,
-  appId: process.env.1:283699367756:web:8f1e0869274cbd5f47c9e9,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
